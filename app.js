eda9341a27b8401fe3e426161f74785222d6406c
sidharth atri <sidharthatri@gmail.com>
2020-09-22 12:30:04 +0530

    display products

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Variables
+//variables
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
@@ -9,14 +9,14 @@ const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
 
-// Cart
+// cart
 let cart = [];
 
-// Getting the product
+// getting the products
 class Products {
   async getProducts() {
     try {
-      let result = await fetch("./products.json");
+      let result = await fetch("products.json");
       let data = await result.json();
 
       let products = data.items;
@@ -33,10 +33,36 @@ class Products {
   }
 }
 
-// Display products
-class UI {}
+// display products
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((product) => {
+      result += `
+     <!-- single product -->
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+        <!--end of single product -->
+     `;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
 
-// Local storage
+//local storage
 class Storage {}
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -44,5 +70,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   // get all products
-  products.getProducts().then((data) => console.log(data));
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
